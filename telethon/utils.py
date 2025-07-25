bf9eef3135945faf3bc02900c61fb85b004befb4
bol4onok <mikhail@MacBook-Pro-Mihail.local>
2025-07-25 20:35:37 +0300

    Revert "Add ChannelForbidden check to utils.get_display_name"
    This reverts commit 6625327b4fb1e90535c54f792adb6061cfadc22c.

--- telethon/utils.py
+++ telethon/utils.py
@@ -95,8 +95,7 @@ def get_display_name(entity):
         else:
             return ''
 
-    elif isinstance(entity, (
-            types.Chat, types.ChatForbidden, types.Channel, types.ChannelForbidden)):
+    elif isinstance(entity, (types.Chat, types.ChatForbidden, types.Channel)):
         return entity.title
 
     return ''
