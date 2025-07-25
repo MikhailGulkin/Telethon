47b577b6d52e20dfd3b9ceab26ed482896a2c184
bol4onok <mikhail@MacBook-Pro-Mihail.local>
2025-07-25 20:35:35 +0300

    Revert "Handle cases when Message.reply_to is MessageReplyStoryHeader"
    This reverts commit 31e8ceeecc02159b2563cde2bfc39b34a8a299cb.

--- telethon/tl/custom/message.py
+++ telethon/tl/custom/message.py
@@ -96,7 +96,7 @@ class Message(ChatGetter, SenderGetter, TLObject):
             The ID of the bot used to send this message
             through its inline mode (e.g. "via @like").
 
-        reply_to (:tl:`MessageReplyHeader` | :tl:`MessageReplyStoryHeader`):
+        reply_to (:tl:`MessageReplyHeader`):
             The original reply header if this message is replying to another.
 
         date (`datetime`):
@@ -379,11 +379,10 @@ class Message(ChatGetter, SenderGetter, TLObject):
     @property
     def is_reply(self):
         """
-        `True` if the message is a reply to some other message or story.
+        `True` if the message is a reply to some other message.
 
-        Remember that if the replied-to is a message, 
-        you can access the ID of the message this one is 
-        replying to through `reply_to.reply_to_msg_id`,
+        Remember that you can access the ID of the message
+        this one is replying to through `reply_to.reply_to_msg_id`,
         and the `Message` object with `get_reply_message()`.
         """
         return self.reply_to is not None
@@ -672,11 +671,7 @@ class Message(ChatGetter, SenderGetter, TLObject):
         Returns the message ID this message is replying to, if any.
         This is equivalent to accessing ``.reply_to.reply_to_msg_id``.
         """
-        return (
-            self.reply_to.reply_to_msg_id
-            if isinstance(self.reply_to, types.MessageReplyHeader)
-            else None
-        )
+        return self.reply_to.reply_to_msg_id if self.reply_to else None
 
     @property
     def to_id(self):
@@ -742,7 +737,7 @@ class Message(ChatGetter, SenderGetter, TLObject):
         The result will be cached after its first use.
         """
         if self._reply_message is None and self._client:
-            if not isinstance(self.reply_to, types.MessageReplyHeader):
+            if not self.reply_to:
                 return None
 
             # Bots cannot access other bots' messages by their ID.
