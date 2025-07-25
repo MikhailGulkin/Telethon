285ead26f8ac0fe2380943df55cdbd5ac83bf653
bol4onok <mikhail@MacBook-Pro-Mihail.local>
2025-07-25 20:35:37 +0300

    Revert "Update FileLike type hint to better reflect valid types (#4620)"
    This reverts commit 663a1808a1be9a799a913a262c1a7af6d51bfab4.

--- telethon/hints.py
+++ telethon/hints.py
@@ -44,12 +44,7 @@ FileLike = typing.Union[
     typing.BinaryIO,
     types.TypeMessageMedia,
     types.TypeInputFile,
-    types.TypeInputFileLocation,
-    types.TypeInputMedia,
-    types.TypePhoto,
-    types.TypeInputPhoto,
-    types.TypeDocument,
-    types.TypeInputDocument
+    types.TypeInputFileLocation
 ]
 
 # Can't use `typing.Type` in Python 3.5.2
